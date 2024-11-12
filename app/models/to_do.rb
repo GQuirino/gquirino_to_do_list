d441bde73197da3dfc63ba39d144927805cbccc0
Guilherme Quirino de Melo <guilhermequirino74@gmail.com>
2024-11-12 15:28:27 -0300

    feature - add ToDo model

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,3 +1,12 @@
 class ToDo < ApplicationRecord
-  enum status: { pending: "pending", complete: "complete" }
+  AVAILABLE_STATUS = {
+    pending: "pending",
+    completed: "completed"
+  }.freeze
+
+  enum :status, AVAILABLE_STATUS
+
+  validates :status, presence: true, inclusion: { in: AVAILABLE_STATUS.values }
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -1,5 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe ToDo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:status) { :pending }
+  let(:title) { "New Title" }
+  let(:description) { "New Description" }
+  let(:to_do_item) { described_class.new(title:, status:, description:) }
+
+  describe "validations" do
+    context "#status" do
+      context "with valid status" do
+        subject { to_do_item }
+
+        it "returns true" do
+          expect(subject).to be_valid
+        end
+
+        it "is valid with a status of 'pending'" do
+          status = :pending
+          expect(subject).to be_valid
+        end
+
+        it "is valid with a status of 'complete'" do
+          status = :complete
+          expect(subject).to be_valid
+        end
+      end
+
+      context "with invalid status" do
+        it "returns false" do
+          status = :invalid_status
+          expect(subject).not_to be_valid
+        end
+
+        it "is not valid without a status" do
+          status = nil
+          expect(subject).not_to be_valid
+          expect(subject.errors[:status]).to include("can't be blank")
+        end
+      end
+    end
+
+    context "#title" do
+      context "with valid title" do
+        subject { to_do_item }
+
+        it "returns true" do
+          expect(subject).to be_valid
+        end
+      end
+
+      context "with invalid title" do
+        it "is not valid without a title" do
+          title = nil
+          expect(subject).not_to be_valid
+          expect(subject.errors[:title]).to include("can't be blank")
+        end
+      end
+    end
+
+    context "#description" do
+      context "with valid description" do
+        subject { to_do_item }
+
+        it "returns true" do
+          expect(subject).to be_valid
+        end
+      end
+
+      context "with invalid description" do
+        it "is not valid without a description" do
+          description = nil
+          expect(subject).not_to be_valid
+          expect(subject.errors[:description]).to include("can't be blank")
+        end
+      end
+    end
+  end
 end
